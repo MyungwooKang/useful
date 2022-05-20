a7c89702a3752f6900e7c8fb70a6a9362b094888
auddn521 <auddn521@gmail.com>
2022-05-20 21:31:34 +0900

    init

---
+++ src/test/java/com/kmw/useful/UsefulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmw.useful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
